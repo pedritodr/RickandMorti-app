ef7ceb0e7991134183b06ba00b4f2709e36f98de
PedritoDR <pedroduran014@gmail.com>
2023-12-06 19:47:12 -0400

    initial

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import CharacterScreen from './src/screens/CharacterScreen';
+
+function App(): JSX.Element {
+  return <CharacterScreen />;
+}
+
+export default App;
---
+++ src/screens/CharacterScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const CharacterScreen = () => {
+  return (
+    <View>
+      <Text>CharacterScreen</Text>
+    </View>
+  );
+};
+
+export default CharacterScreen;
